cb3f307a51f70257d39045c2133e69cb302ec4cb
obloomfield <obloomfield@brown.edu>
2022-12-25 16:01:04 -0500

    lab1 test

---
+++ lab1/tests/q2.py
@@ -0,0 +1,12 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> assert capitalize("a") == "A"\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert capitalize("A") == "A"\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert capitalize("Brenda") == "BRENDA"\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert capitalize("Brenda Rubenstein") == "BRENDA RUBENSTEIN"\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
